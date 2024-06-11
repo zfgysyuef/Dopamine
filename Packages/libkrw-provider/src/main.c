b3f8bdbaf9bbde002716269dc85162fc75702430
opa334 <opa334@users.noreply.github.com>
2024-06-11 22:43:52 +0200

    libkrw bug fixes, bump version of it to 2.0.2

--- Packages/libkrw-provider/src/main.c
+++ Packages/libkrw-provider/src/main.c
@@ -1,8 +1,9 @@
 #include <libjailbreak/libjailbreak.h>
 #include <libkrw/libkrw_plugin.h>
 #include <dispatch/dispatch.h>
+#include <errno.h>
 
-void load_primitives_once(void)
+static void load_primitives_once(void)
 {
 	static dispatch_once_t onceToken;
 	dispatch_once (&onceToken, ^{
@@ -10,33 +11,36 @@ void load_primitives_once(void)
 	});
 }
 
-int kwritebuf_wrapper(void *from, uint64_t to, size_t len)
+static int kwritebuf_wrapper(void *from, uint64_t to, size_t len)
 {
 	return kwritebuf(to, from, len);
 }
 
-int kcall_wrapper(uint64_t func, size_t argc, const uint64_t *argv, uint64_t *ret)
+static int kcall_wrapper(uint64_t func, size_t argc, const uint64_t *argv, uint64_t *ret)
 {
-	return kcall(ret, func, argc, argv);
+	if (jbinfo(usesPACBypass)) {
+		return kcall(ret, func, argc, argv);
+	}
+	return ENOTSUP;
 }
 
-int physreadbuf_wrapper(uint64_t from, void *to, size_t len, uint8_t granule)
+static int physreadbuf_wrapper(uint64_t from, void *to, size_t len, uint8_t granule)
 {
 	return physreadbuf(from, to, len);
 }
 
-int physwritebuf_wrapper(void *from, uint64_t to, size_t len, uint8_t granule)
+static int physwritebuf_wrapper(void *from, uint64_t to, size_t len, uint8_t granule)
 {
 	return physwritebuf(to, from, len);
 }
 
-int kbase_wrapper(uint64_t *kbase)
+static int kbase_wrapper(uint64_t *kbase)
 {
 	*kbase = kconstant(base);
 	return 0;
 }
 
-__attribute__((used)) krw_plugin_initializer_t krw_initializer(krw_handlers_t handlers)
+__attribute__((used)) int krw_initializer(krw_handlers_t handlers)
 {
 	load_primitives_once();
 
@@ -48,14 +52,11 @@ __attribute__((used)) krw_plugin_initializer_t krw_initializer(krw_handlers_t ha
 	return 0;
 }
 
-__attribute__((used)) krw_plugin_initializer_t kcall_initializer(krw_handlers_t handlers)
+__attribute__((used)) int kcall_initializer(krw_handlers_t handlers)
 {
 	load_primitives_once();
 
-	if (jbinfo(usesPACBypass)) {
-		handlers->kcall = kcall_wrapper;
-	}
-
+	handlers->kcall = kcall_wrapper;
 	handlers->physread = physreadbuf_wrapper;
 	handlers->physwrite = physwritebuf_wrapper;
 	return 0;
